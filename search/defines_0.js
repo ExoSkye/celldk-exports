741a290e9dd678d69a062d56adbd78a0d9825e42
ProtoByter <bobrow2006@gmail.com>
2022-04-17 08:59:01 +0000

    Deploying to gh-pages from  @ 7392581738ffa67e0091c0ac73604e90a548b9bb 🚀

--- search/defines_0.js
+++ search/defines_0.js
@@ -1,8 +1,11 @@
 var searchData=
 [
-  ['sys_5fprocess_5fexit_5fid_21',['SYS_PROCESS_EXIT_ID',['../sys__process_8h.html#a3befbce5c743c73857dc6b2136cbf6d0',1,'sys_process.h']]],
-  ['sys_5fprocess_5fget_5fsdk_5fversion_5fid_22',['SYS_PROCESS_GET_SDK_VERSION_ID',['../sys__process_8h.html#a3ea7fcaaa83e68dcbc3bd48c67b775dd',1,'sys_process.h']]],
-  ['sys_5fprocess_5fgetpid_5fid_23',['SYS_PROCESS_GETPID_ID',['../sys__process_8h.html#a1a0d43021b8ea5b710fa9f0606e55ad5',1,'sys_process.h']]],
-  ['sys_5ftty_5fread_5fid_24',['SYS_TTY_READ_ID',['../sys__tty_8h.html#a73fd79b78e8b61252cf71c51abb880d9',1,'sys_tty.h']]],
-  ['sys_5ftty_5fwrite_5fid_25',['SYS_TTY_WRITE_ID',['../sys__tty_8h.html#a07be72067a5d0be3cd93b69819961bf9',1,'sys_tty.h']]]
+  ['sys_5ffs_5fclose_5fid_32',['SYS_FS_CLOSE_ID',['../sys__fs_8h.html#aaaacc770f1f0f00d3c4dd673aceae9be',1,'sys_fs.h']]],
+  ['sys_5ffs_5fopen_5fid_33',['SYS_FS_OPEN_ID',['../sys__fs_8h.html#aab737c0e99e194adf82558e58a653849',1,'sys_fs.h']]],
+  ['sys_5ffs_5fread_5fid_34',['SYS_FS_READ_ID',['../sys__fs_8h.html#a79bff48ac74630eef4cf9bd2e3c511c7',1,'sys_fs.h']]],
+  ['sys_5fprocess_5fexit_5fid_35',['SYS_PROCESS_EXIT_ID',['../sys__process_8h.html#a3befbce5c743c73857dc6b2136cbf6d0',1,'sys_process.h']]],
+  ['sys_5fprocess_5fget_5fsdk_5fversion_5fid_36',['SYS_PROCESS_GET_SDK_VERSION_ID',['../sys__process_8h.html#a3ea7fcaaa83e68dcbc3bd48c67b775dd',1,'sys_process.h']]],
+  ['sys_5fprocess_5fgetpid_5fid_37',['SYS_PROCESS_GETPID_ID',['../sys__process_8h.html#a1a0d43021b8ea5b710fa9f0606e55ad5',1,'sys_process.h']]],
+  ['sys_5ftty_5fread_5fid_38',['SYS_TTY_READ_ID',['../sys__tty_8h.html#a73fd79b78e8b61252cf71c51abb880d9',1,'sys_tty.h']]],
+  ['sys_5ftty_5fwrite_5fid_39',['SYS_TTY_WRITE_ID',['../sys__tty_8h.html#a07be72067a5d0be3cd93b69819961bf9',1,'sys_tty.h']]]
 ];
