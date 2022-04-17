a37de266cfd380623e64b546896205b9ac1bab4d
ProtoByter <bobrow2006@gmail.com>
2022-04-17 18:59:24 +0000

    Deploying to gh-pages from  @ 95ca6eaf07990bac03c7c71fe10a354d34e15d3f 🚀

--- search/files_0.js
+++ search/files_0.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['sys_5ffs_2eh_20',['sys_fs.h',['../sys__fs_8h.html',1,'']]],
-  ['sys_5fprocess_2eh_21',['sys_process.h',['../sys__process_8h.html',1,'']]],
-  ['sys_5ftty_2eh_22',['sys_tty.h',['../sys__tty_8h.html',1,'']]],
-  ['syscalls_2eh_23',['syscalls.h',['../syscalls_8h.html',1,'']]]
+  ['sys_5ffs_2eh_26',['sys_fs.h',['../sys__fs_8h.html',1,'']]],
+  ['sys_5fprocess_2eh_27',['sys_process.h',['../sys__process_8h.html',1,'']]],
+  ['sys_5ftty_2eh_28',['sys_tty.h',['../sys__tty_8h.html',1,'']]],
+  ['syscalls_2eh_29',['syscalls.h',['../syscalls_8h.html',1,'']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,11 +1,14 @@
 var searchData=
 [
-  ['sys_5ffs_5fclose_24',['sys_fs_close',['../sys__fs_8h.html#a24390bb766457c498e00b2dac4f24fad',1,'sys_fs.h']]],
-  ['sys_5ffs_5fopen_25',['sys_fs_open',['../sys__fs_8h.html#a88fd1573b8452b7bf0f1f5a7f7754463',1,'sys_fs.h']]],
-  ['sys_5ffs_5fread_26',['sys_fs_read',['../sys__fs_8h.html#ab4964b157afdd3a9432f28e722e0a718',1,'sys_fs.h']]],
-  ['sys_5fprocess_5fexit_27',['sys_process_exit',['../sys__process_8h.html#af1b464864f9fab6e0f260fdef4b4c225',1,'sys_process.h']]],
-  ['sys_5fprocess_5fget_5fsdk_5fversion_28',['sys_process_get_sdk_version',['../sys__process_8h.html#a74d656709758c0173a0625111f9a3191',1,'sys_process.h']]],
-  ['sys_5fprocess_5fgetpid_29',['sys_process_getpid',['../sys__process_8h.html#ab320916c77f69cc63b6f7f88e6f82d62',1,'sys_process.h']]],
-  ['sys_5ftty_5fread_30',['sys_tty_read',['../sys__tty_8h.html#ac2dc9fd4427c700a0969ffe78fd3ae53',1,'sys_tty.h']]],
-  ['sys_5ftty_5fwrite_31',['sys_tty_write',['../sys__tty_8h.html#a7d2588023f66c8f89e4a5d7abf882b72',1,'sys_tty.h']]]
+  ['sys_5ffs_5fclose_30',['sys_fs_close',['../sys__fs_8h.html#a24390bb766457c498e00b2dac4f24fad',1,'sys_fs.h']]],
+  ['sys_5ffs_5flseek_31',['sys_fs_lseek',['../sys__fs_8h.html#acf49fffb62da1ebdc2c76aee162096a2',1,'sys_fs.h']]],
+  ['sys_5ffs_5fopen_32',['sys_fs_open',['../sys__fs_8h.html#a88fd1573b8452b7bf0f1f5a7f7754463',1,'sys_fs.h']]],
+  ['sys_5ffs_5fread_33',['sys_fs_read',['../sys__fs_8h.html#ab4964b157afdd3a9432f28e722e0a718',1,'sys_fs.h']]],
+  ['sys_5ffs_5frename_34',['sys_fs_rename',['../sys__fs_8h.html#ac77810254fe8ab500a2394f162b8a17d',1,'sys_fs.h']]],
+  ['sys_5ffs_5funlink_35',['sys_fs_unlink',['../sys__fs_8h.html#a83e5a1f551f06fa89c8aa763bc8c2743',1,'sys_fs.h']]],
+  ['sys_5fprocess_5fexit_36',['sys_process_exit',['../sys__process_8h.html#af1b464864f9fab6e0f260fdef4b4c225',1,'sys_process.h']]],
+  ['sys_5fprocess_5fget_5fsdk_5fversion_37',['sys_process_get_sdk_version',['../sys__process_8h.html#a74d656709758c0173a0625111f9a3191',1,'sys_process.h']]],
+  ['sys_5fprocess_5fgetpid_38',['sys_process_getpid',['../sys__process_8h.html#ab320916c77f69cc63b6f7f88e6f82d62',1,'sys_process.h']]],
+  ['sys_5ftty_5fread_39',['sys_tty_read',['../sys__tty_8h.html#ac2dc9fd4427c700a0969ffe78fd3ae53',1,'sys_tty.h']]],
+  ['sys_5ftty_5fwrite_40',['sys_tty_write',['../sys__tty_8h.html#a7d2588023f66c8f89e4a5d7abf882b72',1,'sys_tty.h']]]
 ];
